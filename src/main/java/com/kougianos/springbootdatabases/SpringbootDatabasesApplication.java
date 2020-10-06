e6691c0d207e302f7db1637e398ec9a9a862affd
Nikos Kougianos <kougi_jr@hotmail.com>
2020-10-06 10:08:44 +0300

    Spring Initializer: project setup

---
+++ src/main/java/com/kougianos/springbootdatabases/SpringbootDatabasesApplication.java
@@ -0,0 +1,13 @@
+package com.kougianos.springbootdatabases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatabasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kougianos/springbootdatabases/SpringbootDatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kougianos.springbootdatabases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatabasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
