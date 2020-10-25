d66d20113dc6d683d70a35fd8ab78ffc217a046a
Nikos Kougianos <kougi_jr@hotmail.com>
2020-10-25 23:30:58 +0200

    Create UserServiceImpl (not implemented yet)

---
+++ src/main/java/com/kougianos/springbootdatabases/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.kougianos.springbootdatabases.service;
+
+import com.kougianos.springbootdatabases.dto.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    public Integer createUser(User user) {
+        return null;
+    }
+
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    public User getUser(Integer id) {
+        return null;
+    }
+
+    public Integer updateUser(Integer id, User user) {
+        return null;
+    }
+
+    public Integer deleteUser(Integer id) {
+        return null;
+    }
+
+    public boolean deleteAllUsers() {
+        return false;
+    }
+}
